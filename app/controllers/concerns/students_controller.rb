5c403efd04ac19be9fac5e6578a7cd06c8511448
Kumaresh <kumareshme2@gmail.com>
2020-12-28 18:17:16 +0530

    DRY

--- app/controllers/concerns/students_controller.rb
+++ app/controllers/concerns/students_controller.rb
@@ -1,5 +1,52 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :edit, :update, :destroy]
+
   def show
+  end
+
+  def index
+    @student = Student.all
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def edit
+  end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:notice] = "Your Details was saved successfully"
+      redirect_to student_path(@student)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @student.update(student_params)
+      flash[:notice] = "Your Details was updated successfully"
+      redirect_to @student
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @student.destroy
+    redirect_to students_path
+  end
+
+  private
+
+  def set_student
     @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:student_name, :email_id, 
+                                   :address, :contact_number, :marks)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  resources :students, only: [:show] 
+  resources :students
 end
