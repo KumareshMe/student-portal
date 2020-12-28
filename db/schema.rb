64c2f26f1233f6c9191d79b920f3fca5a1d7e854
Kumaresh <kumareshme2@gmail.com>
2020-12-28 19:11:24 +0530

    user_create

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_28_085640) do
+ActiveRecord::Schema.define(version: 2020_12_28_133600) do
 
   create_table "students", force: :cascade do |t|
     t.string "student_name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2020_12_28_085640) do
     t.text "address"
     t.integer "contact_number"
     t.integer "marks"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
