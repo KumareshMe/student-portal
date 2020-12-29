09d40f3f70670c223bdb2d2a75d4236070adb87a
Kumaresh <kumareshme2@gmail.com>
2020-12-29 10:13:32 +0530

    User Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, length: { maximum: 35}, format: { with: VALID_EMAIL_REGEX }
 end
